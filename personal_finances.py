54740b2d9dce57bb2cfcd62f4f860f40dc80a711
thiago.novi <thiagonovi6@gmail.com>
2022-11-29 20:55:10 -0300

    Implemented the 'inteligent search' feature
    Stiil have some bugs

--- personal_finances.py
+++ personal_finances.py
@@ -108,6 +108,55 @@ def check_statement():
     input("Press any key to go back to the menu.")
     menu()
 
+
+def search_function(days_behind, description):
+    date = datetime.date.today() - timedelta(days=days_behind)
+    with open("statement.txt") as statement:
+            lines = statement.readlines()
+    while ((str(date) + '\n') not in lines) and (date <= datetime.date.today()):
+        date = date + timedelta(days=1)
+    n = lines.index(str(date) + '\n')
+    all_expense = Decimal(0.00)
+    cleanConsole()
+    while n <= (len(lines) - 3):
+        if lines[n + 1] == (description + '\n'):
+            all_expense = all_expense + Decimal(lines[n + 2])
+            print("%10s"%(lines[n].rstrip('\n')),
+            "%10s"%(lines[n + 1].rstrip('\n')),
+            "%10s"%(lines[n + 2].rstrip('\n')))
+            n += 3
+        else:
+            n += 3
+    print(f"\nYour total expense with '{description.rstrip()}' in the last {days_behind} days:\n")
+    print('R$' + str(all_expense * -1) + '\n')
+
+def expense_search(description):
+    with open("statement.txt") as statement:
+        lines = statement.readlines()
+    print("""Choose the time period for you search:
+    > Last 2 days
+    > Last 5 days
+    > Last 30 days
+    > Last 60 days\n""")
+    statement_period = input(prompt)
+    if len(lines) == 1:
+        cleanConsole()
+        print("You don't have any entrances in your statement yet.")
+    elif statement_period == "2":
+        search_function(2, description)
+    elif statement_period == "5":
+        search_function(5, description)
+    elif statement_period == "30":
+        search_function(30, description)
+    elif statement_period == "60":
+        search_function(60, description)
+    else:
+        cleanConsole()
+        print("Plese, select a valid option.\n")
+        expense_search()
+    input("Press any key to go back to the menu.")
+    menu()
+
 # Function of the menu
 def menu():
     cleanConsole()
@@ -117,7 +166,8 @@ What do you want do do?
     2. Register an expense
     3. Register a gain
     4. Check your statement
-    5. CLose the app""")
+    5. Expense search
+    6. CLose the app""")
     response = input(prompt)
 
     if response == "1":
@@ -138,6 +188,11 @@ What do you want do do?
         cleanConsole()
         check_statement()
     elif response == "5":
+        cleanConsole()
+        print("Add a expense description for the search:")
+        description = input(prompt)
+        expense_search(description)
+    elif response == "6":
         cleanConsole()
         print("Are you sure you want to closse the app? [y/n]")
         close_response = input(prompt)
